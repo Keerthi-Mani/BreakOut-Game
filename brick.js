f7ccdf2da6ae8d932c9af56d9a03e2a8180aab4e
Keerthi Mani <carkeane.getz@gmail.com>
2020-01-08 09:25:35 -0500

    function is called for every 10milliseconds

--- brick.js
+++ brick.js
@@ -3,23 +3,12 @@ var canvas = document.getElementById("myCanvas");
 //Creating a variable to store the 2D rendering context
 var ctx = canvas.getContext("2d");
 
-//Piece of code that prints a red square on the canvas
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50); //(Left Top Width Height)
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
-
-//Piece of code that prints a green circle on the canvas
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
-
-//The code prints a blue-stroked empty rectangle.
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+//function will be called every 10 milliseconds forever, or until we stop it
+function draw() {
+  ctx.beginPath();
+  ctx.arc(50, 50, 10, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+setInterval(draw, 10);
