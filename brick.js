62215fd242314a153f64899599161219f2d572c5
Keerthi Mani <carkeane.getz@gmail.com>
2020-01-08 09:30:39 -0500

    the ball is leaving a trail behind it

--- brick.js
+++ brick.js
@@ -2,13 +2,21 @@
 var canvas = document.getElementById("myCanvas");
 //Creating a variable to store the 2D rendering context
 var ctx = canvas.getContext("2d");
+//To define the position the circle is drawn at
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+//To make it appear that the ball is moving
+var dx = 2;
+var dy = -2;
 
 //function will be called every 10 milliseconds forever, or until we stop it
 function draw() {
   ctx.beginPath();
-  ctx.arc(50, 50, 10, 0, Math.PI * 2);
+  ctx.arc(x, y, 10, 0, Math.PI * 2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
+  x += dx;
+  y += dy;
 }
 setInterval(draw, 10);
